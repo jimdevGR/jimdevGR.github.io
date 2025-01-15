4625c97ff41eca390b44b7f5c6a3d0875bcc9159
jimdevGR <karagiannisj01@gmail.com>
2025-01-15 14:00:43 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import bodyParser from 'body-parser';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -18,8 +19,8 @@ app.listen(port, () => {
 });
 
 // Διάβασμα των συνδέσμων και των εκθέσεων
-let total_links = JSON.parse(fs.readFileSync(__dirname + '/links.json'));
-let total_exhibitions = JSON.parse(fs.readFileSync(__dirname + '/exhibitions.json'));
+let total_links = JSON.parse(fs.readFileSync(path.join(__dirname, 'links.json')));
+let total_exhibitions = JSON.parse(fs.readFileSync(path.join(__dirname, 'exhibitions.json')));
 
 // Αποστολή των συνδέσμων και εκθέσεων σε μορφή json
 app.get('/api/links', (req, res) => res.json(total_links));
